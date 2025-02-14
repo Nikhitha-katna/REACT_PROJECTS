8bfb0fe141f721d749a7f96262250e4f658f5f0a
Nikhitha Katna <katnanikhitha@gmail.com>
2025-02-14 19:49:15 +0530

    Update Home.jsx

--- 09blogapp/src/pages/Home.jsx
+++ 09blogapp/src/pages/Home.jsx
@@ -1,46 +1,64 @@
-import React, {useEffect, useState} from 'react'
-import service from "../appwrite/config";
-import {Container, PostCard} from '../components'
+
+     import React, { useEffect, useState } from 'react';
+import service from '../appwrite/config';
+import { Container, PostCard } from '../components';
 
 function Home() {
-    const [posts, setPosts] = useState([])
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        service.getPosts().then((posts) => {
-            if (posts) {
-                setPosts(posts.documents)
-            }
-        })
-    }, [])
-  
-    if (posts.length === 0) {
-        return (
-            <div className="w-full py-8 mt-4 text-center">
-                <Container>
-                    <div className="flex flex-wrap">
-                        <div className="p-2 w-full">
-                            <h1 className="text-2xl font-bold hover:text-gray-500">
-                                Login to read posts
-                            </h1>
-                        </div>
-                    </div>
-                </Container>
-            </div>
-        )
+  useEffect(() => {
+    async function fetchPosts() {
+      try {
+        const response = await service.getPosts();
+        if (response) {
+          setPosts(response.documents);
+        }
+      } catch (error) {
+        console.error("Failed to fetch posts:", error);
+      } finally {
+        setLoading(false);
+      }
     }
+
+    fetchPosts();
+  }, []);
+
+  if (loading) {
     return (
-        <div className='w-full py-8'>
-            <Container>
-                <div className='flex flex-wrap'>
-                    {posts.map((post) => (
-                        <div key={post.$id} className='p-2 w-1/4'>
-                            <PostCard {...post} />
-                        </div>
-                    ))}
-                </div>
-            </Container>
+      <div className="w-full py-8 mt-4 text-center">
+        <Container>
+          <h1 className="text-2xl font-bold">Loading posts...</h1>
+        </Container>
+      </div>
+    );
+  }
+
+  if (posts.length === 0) {
+    return (
+      <div className="w-full py-8 mt-4 text-center">
+        <Container>
+          <h1 className="text-2xl font-bold hover:text-gray-500">
+            No posts available. Login to read posts.
+          </h1>
+        </Container>
+      </div>
+    );
+  }
+
+  return (
+    <div className="w-full py-8">
+      <Container>
+        <div className="flex flex-wrap -mx-2">
+          {posts.map((post) => (
+            <div key={post.$id} className="p-2 w-full sm:w-1/2 md:w-1/3 lg:w-1/4">
+              <PostCard {...post} />
+            </div>
+          ))}
         </div>
-    )
+      </Container>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
